bdb17be89ebefdbc6b6745145eff8342164635d0
Ryan.K <ryankung@ieee.org>
2018-03-29 16:48:50 +0800

    modified config

--- truffle.js
+++ truffle.js
@@ -15,7 +15,6 @@ module.exports = {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*", // Match any network id,
-            from: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
         }
     }
 };
