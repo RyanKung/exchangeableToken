a592de5384f3ca56686be849a724d2ebe8761690
Ryan.K <ryankung@ieee.org>
2018-04-13 18:30:42 +0800

    using interface

--- contracts/ExchangeableERC20.sol
+++ contracts/ExchangeableERC20.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.4.17;
 
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol';
+import './Exchangeable.sol';
 
-contract ExchangeableERC20 is StandardToken, DetailedERC20 {
+contract ExchangeableERC20 is StandardToken, DetailedERC20, Exchangeable {
   using SafeMath for uint256;
 
   uint256 public bidTickerId = 0;
@@ -20,20 +21,9 @@ contract ExchangeableERC20 is StandardToken, DetailedERC20 {
   mapping(uint256 => Ticker) public bidTable;
   mapping(uint256 => Ticker) public askTable;
 
-  function deleteTicker(Ticker storage _t) private returns (bool);
-  function updateAmount(Ticker storage _t, uint256 _amount) private returns (bool);
   function increaseBid() private returns (bool);
   function increaseAid() private returns (bool);
-  function checkBidTicker(uint256 _id) public view returns (address _addr,uint256 _price,uint256 _amount);
-  function checkAskTicker(uint256 _id) public view returns (address _addr,uint256 _price,uint256 _amount);
-  function matchBid(uint256 _price, uint256 _start) public view returns (uint256);
-  function matchAsk(uint256 _price, uint256 _start) public view returns (uint256);
-  function ask (uint256 _price, uint256 _amount) public returns (uint256);
-  function bid (uint256 _price, uint256 _amount) public payable returns (uint256);
-  function fillBidOrAsk(uint256 _id, uint256 _price, uint256 _amount) public returns (uint256);
-  function fillAskOrBid(uint256 _id, uint256 _price, uint256 _amount) public payable returns (uint256);
-  function fillBid(uint256 _id, uint256 _price, uint256 _amount) public returns (uint256);
-  function fillAsk(uint256 _id, uint256 _price, uint256 _amount) public payable returns (uint256);
-  function cancelAsk(uint256 _id) public returns (bool);
-  function cancelBid(uint256 _id) public returns (bool);
+  function deleteTicker(Ticker storage _t) private returns (bool);
+  function updateAmount(Ticker storage _t, uint256 _amount) private returns (bool);
+
 }
